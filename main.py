2960a437c8ac660b9b3ca692c5ca83f513c9d5fc
AntoDono <anthony20151128@gmail.com>
2023-04-24 19:05:20 -0400

    rename columns only

--- main.py
+++ main.py
@@ -6,50 +6,33 @@ import os
 from os import listdir
 from os.path import isfile, join
 from dotenv import load_dotenv
+import warnings
+
+warnings.filterwarnings(
+    action='ignore',
+    message='The error_bad_lines argument has been deprecated',
+)
 
 load_dotenv()
 BASE_DIR = os.getcwd()
-include_columns = eval(os.getenv("INCLUDE_COL_NAMES"))
 rename_col = os.getenv("RENAME_COL").lower() == "true"
 rename_columns = eval(os.getenv("RENAME_COL_NAMES"))
 csv_seperator = os.getenv("SEPARATOR")
 
-if (len(include_columns)!=len(rename_columns) and rename_col): raise Exception("INCLUDE_COL_NAMES length does not match RENAME_COL_NAMES length.")
-
 raw_data_files = [f for f in listdir(f"{BASE_DIR}/raw-data") if (isfile(join(f"{BASE_DIR}/raw-data", f)) and f!="placeholder.txt" )]
 
-combined_data = None
-
 for filename in raw_data_files:
-    print(filename)
+    print(filename, end=" - ")
     data = pd.read_csv(f"{BASE_DIR}/raw-data/{filename}", sep=csv_seperator, error_bad_lines=False)
 
-    if (type(combined_data)!=pd.DataFrame):
-        combined_data = data
-    else:
-        combined_data = pd.concat([data, combined_data])
+    if (rename_col):
+        name1 = data.loc[0, data.columns[0]]
+        name2 = data.loc[1, data.columns[0]]
 
+        if (name1 != "Participant" and name2 != "Participant"):
+            print(f"{filename} DOES NOT HAVE 'Participant'")
 
-# print(combined_data)
+        data.columns = ["name", "line"]
 
-for col in combined_data.columns.to_list():
-    if col not in include_columns:
-        combined_data.drop(col, axis=1, inplace=True)
-
-
-if (rename_col):
-    col = {}
-    for i in range(0,len(rename_columns)):
-        col[include_columns[i]] = rename_columns[i]
-    combined_data.rename(columns=col, inplace=True, errors='raise')
-
-# count = 0
-# for index, row in combined_data.iterrows():
-#     if count%2 == 0:
-#         row['name'] = 'Participant'
-#     else:
-#         row['name'] = 'Ellie'
-#     count += 1
-
-
-combined_data.to_csv(f"{BASE_DIR}/combined.csv", index=None, header=True)
+    data.to_csv(f"{BASE_DIR}/output/{filename}", index=None, header=True)
+    print("Done")
